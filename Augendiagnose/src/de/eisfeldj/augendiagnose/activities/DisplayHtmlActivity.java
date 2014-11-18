a7b358825751f9fa4034522d2d74b6d8acd690a3
Jörg Eisfeld <github@jeisfeld.de>
2014-11-18 20:07:24 +0100

    Enable navigation within help screens

--- Augendiagnose/src/de/eisfeldj/augendiagnose/activities/DisplayHtmlActivity.java
+++ Augendiagnose/src/de/eisfeldj/augendiagnose/activities/DisplayHtmlActivity.java
@@ -4,8 +4,12 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import de.eisfeldj.augendiagnose.Application;
 import de.eisfeldj.augendiagnose.R;
 import de.eisfeldj.augendiagnose.fragments.DisplayHtmlFragment;
+import de.eisfeldj.augendiagnose.fragments.DisplayHelpNavigatonFragment;
 
 /**
  * Activity to display an HTML page. Used for display of help pages.
@@ -18,9 +22,18 @@ public class DisplayHtmlActivity extends Activity {
 	private static final String STRING_EXTRA_RESOURCE = "de.eisfeldj.augendiagnose.RESOURCE";
 
 	/**
-	 * The fragment tag.
+	 * Resource id indicating that there is no resource.
+	 */
+	private static final int NO_RESOURCE = -1;
+
+	/**
+	 * The main fragment tag.
 	 */
 	private static final String FRAGMENT_TAG = "FRAGMENT_TAG";
+	/**
+	 * The details fragment tag.
+	 */
+	private static final String FRAGMENT_DETAILS_TAG = "FRAGMENT_DETAILS_TAG";
 
 	/**
 	 * Static helper method to start the activity, passing the resource holding the HTML as string.
@@ -36,21 +49,93 @@ public class DisplayHtmlActivity extends Activity {
 		context.startActivity(intent);
 	}
 
+	/**
+	 * Static helper method to start the activity without resource - just start navigation.
+	 *
+	 * @param context
+	 *            The context in which the activity is started.
+	 */
+	public static void startActivity(final Context context) {
+		Intent intent = new Intent(context, DisplayHtmlActivity.class);
+		context.startActivity(intent);
+	}
+
 	@Override
 	protected final void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 
-		int resource = getIntent().getIntExtra(STRING_EXTRA_RESOURCE, -1);
+		int resource = getIntent().getIntExtra(STRING_EXTRA_RESOURCE, NO_RESOURCE);
 
-		setContentView(R.layout.activity_fragments_single);
+		if (Application.isTablet()) {
+			setContentView(R.layout.activity_fragments_list_detail);
+		}
+		else {
+			setContentView(R.layout.activity_fragments_single);
+		}
 
-		if (getFragmentManager().findFragmentByTag(FRAGMENT_TAG) == null) {
-			DisplayHtmlFragment fragment = new DisplayHtmlFragment();
-			fragment.setParameters(resource);
+		if (Application.isTablet() || resource == NO_RESOURCE) {
+			displayNavigation();
+		}
 
-			getFragmentManager().beginTransaction().add(R.id.fragment_container, fragment, FRAGMENT_TAG).commit();
-			getFragmentManager().executePendingTransactions();
+		if (resource != NO_RESOURCE) {
+			displayDetails(resource);
+		}
+	}
+
+	/**
+	 * Display the navigation page.
+	 */
+	public final void displayNavigation() {
+		int containerViewId = Application.isTablet() ? R.id.fragment_list : R.id.fragment_container;
+		DisplayHelpNavigatonFragment fragment = new DisplayHelpNavigatonFragment();
+		getFragmentManager().beginTransaction()
+				.replace(containerViewId, fragment, FRAGMENT_TAG)
+				.commit();
+		getFragmentManager().executePendingTransactions();
+	}
+
+	/**
+	 * Display a details HTML page.
+	 *
+	 * @param resourceId
+	 *            The resource to be shown in the details.
+	 */
+	public final void displayDetails(final int resourceId) {
+		int containerViewId = Application.isTablet() ? R.id.fragment_detail : R.id.fragment_container;
+		String fragmentTag = Application.isTablet() ? FRAGMENT_DETAILS_TAG : FRAGMENT_TAG;
+
+		DisplayHtmlFragment detailFragment = new DisplayHtmlFragment();
+		detailFragment.setParameters(resourceId);
+		getFragmentManager().beginTransaction()
+				.replace(containerViewId, detailFragment, fragmentTag)
+				.commit();
+		getFragmentManager().executePendingTransactions();
+	}
+
+	/*
+	 * Inflate options menu.
+	 */
+	@Override
+	public final boolean onCreateOptionsMenu(final Menu menu) {
+		if (!Application.isTablet()) {
+			// Allow navigation to help overview page on smartphones
+			getMenuInflater().inflate(R.menu.menu_only_help, menu);
+		}
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	/*
+	 * Handle menu actions.
+	 */
+	@Override
+	public final boolean onOptionsItemSelected(final MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.action_help:
+			displayNavigation();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
 		}
 	}
 
