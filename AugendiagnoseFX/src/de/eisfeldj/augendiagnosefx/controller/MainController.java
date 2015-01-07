632198540a489c63a62a16e59c79dd1885849c60
Jörg Eisfeld <github@jeisfeld.de>
2015-01-07 03:02:13 +0100

    Included image directly in fxgraph

--- AugendiagnoseFX/src/de/eisfeldj/augendiagnosefx/controller/MainController.java
+++ AugendiagnoseFX/src/de/eisfeldj/augendiagnosefx/controller/MainController.java
@@ -1,29 +1,24 @@
 package de.eisfeldj.augendiagnosefx.controller;
 
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuBar;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import de.eisfeldj.augendiagnosefx.util.FXMLUtil;
 import de.eisfeldj.augendiagnosefx.util.Logger;
-import de.eisfeldj.augendiagnosefx.util.ResourceUtil;
 
 /**
  * The controller of the main window.
  */
-public class MainController extends BaseController implements Initializable {
+public class MainController extends BaseController {
 	/**
 	 * The root of the main page.
 	 */
@@ -67,11 +62,6 @@ public class MainController extends BaseController implements Initializable {
 	@Override
 	public final Parent getRoot() {
 		return mainPane;
-	}
-
-	@Override
-	public final void initialize(final URL location, final ResourceBundle resources) {
-		closeButton.setGraphic(new ImageView(ResourceUtil.getImage("close.png")));
 	}
 
 	/**
