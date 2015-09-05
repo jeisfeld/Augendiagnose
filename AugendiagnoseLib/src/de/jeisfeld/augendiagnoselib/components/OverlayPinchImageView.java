cbc30ba3fabd1d99b7a3b472b5755ba4263535de
Jörg Eisfeld <github@jeisfeld.de>
2015-09-05 17:22:36 +0200

    Make number of overlays dependent on authorization status.

--- AugendiagnoseLib/src/de/jeisfeld/augendiagnoselib/components/OverlayPinchImageView.java
+++ AugendiagnoseLib/src/de/jeisfeld/augendiagnoselib/components/OverlayPinchImageView.java
@@ -29,6 +29,7 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import de.jeisfeld.augendiagnoselib.Application;
+import de.jeisfeld.augendiagnoselib.Application.AuthorizationLevel;
 import de.jeisfeld.augendiagnoselib.R;
 import de.jeisfeld.augendiagnoselib.util.PreferenceUtil;
 import de.jeisfeld.augendiagnoselib.util.imagefile.EyePhoto;
@@ -47,7 +48,9 @@ public class OverlayPinchImageView extends PinchImageView {
 	/**
 	 * The number of overlays (including circle).
 	 */
-	public static final int OVERLAY_COUNT = Application.getAppContext().getResources().getIntArray(R.array.overlay_types).length;
+	public static final int OVERLAY_COUNT = Application.getAuthorizationLevel() == AuthorizationLevel.TRIAL_ACCESS
+			? Integer.parseInt(Application.getResourceString(R.string.overlay_trial_count))
+			: Application.getAppContext().getResources().getIntArray(R.array.overlay_types).length;
 
 	/**
 	 * The size of the overlays (in pixels).
