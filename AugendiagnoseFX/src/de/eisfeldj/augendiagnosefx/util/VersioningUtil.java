af7b3b9b1f41d127b4e62752aae351e3395906a5
Jörg Eisfeld <github@jeisfeld.de>
2017-02-11 23:56:37 +0100

    Initial preparation for FX version 1.1.11

--- AugendiagnoseFX/src/de/eisfeldj/augendiagnosefx/util/VersioningUtil.java
+++ AugendiagnoseFX/src/de/eisfeldj/augendiagnosefx/util/VersioningUtil.java
@@ -27,7 +27,7 @@ public final class VersioningUtil {
 	/**
 	 * The current version of the application.
 	 */
-	public static final VersionInfo CURRENT_VERSION = new VersionInfo(11, "0.1.10");
+	public static final VersionInfo CURRENT_VERSION = new VersionInfo(12, "0.1.11");
 
 	/**
 	 * Base URL for downloading the application.
