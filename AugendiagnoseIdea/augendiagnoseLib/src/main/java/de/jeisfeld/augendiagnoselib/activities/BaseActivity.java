3f25f271f127c8580da1b22a7056e9707ac324a4
Jörg Eisfeld <github@jeisfeld.de>
2017-04-23 12:35:15 +0200

    Fix potential exception

--- AugendiagnoseIdea/augendiagnoseLib/src/main/java/de/jeisfeld/augendiagnoselib/activities/BaseActivity.java
+++ AugendiagnoseIdea/augendiagnoseLib/src/main/java/de/jeisfeld/augendiagnoselib/activities/BaseActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
-import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -25,12 +24,12 @@ public abstract class BaseActivity extends Activity {
 	protected void onCreate(@Nullable final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		// Update title
+		// Update title - required for custom locale on Android N
 		try {
 			ActivityInfo activityInfo = getPackageManager().getActivityInfo(getComponentName(), PackageManager.GET_META_DATA);
 			setTitle(activityInfo.labelRes);
 		}
-		catch (NameNotFoundException ex) {
+		catch (Exception ex) {
 			Log.e(Application.TAG, "Error while getting activity info. " + ex.getMessage(), ex);
 		}
 	}
